67b00a691aae20d39b9a6692475813ec75590ec3
GSpence <spencegarett@gmail.com>
2014-04-01 18:22:01 -0400

    Final

--- crm.rb
+++ crm.rb
@@ -2,28 +2,25 @@ require_relative 'contact'
 require_relative 'rolodex'
 require 'sinatra'
 
+@@rolodex = Rolodex.new
+
 get '/' do
 	@crm_app_name = "My CRM"
 	erb :index
 end
 
 get '/contacts' do
-	@contacts = []
-	@contacts << Contact.new("Julie", "Hache", "julie@bitmakerlabs.com", "Instructor")
-  	@contacts << Contact.new("Will", "Richman", "will@bitmakerlabs.com", "Co-Founder")
-  	@contacts << Contact.new("Chris", "Jo	hnston", "chris@bitmakerlabs.com", "Instructor")
 	erb :contacts
 end
 
 get '/contacts/new' do
-	erb:index
+	erb :new_contact
 end
 
-get 'contacts/:id' do
-	erb:index
+post '/contacts' do
+	new_contact = Contact.new(params[:first_name], params[:last_name], params[:email], params[:note])
+	@@rolodex.add_contact(new_contact)
+	redirect to ('/contacts')
 end
 
-get 'contact/:id/edit' do
-	erb:index
-end
 
