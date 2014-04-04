fcf1139c44d5515334779121823649ed69c741fa
GSpence <spencegarett@gmail.com>
2014-04-04 15:25:18 -0400

    Put Form

--- crm.rb
+++ crm.rb
@@ -27,10 +27,23 @@ get '/contacts/:id' do
 	end
 end
 
-get 'contacts/:id/edit' do
+get '/contacts/:id/edit' do
 	@contact = @@rolodex.find(params[:id].to_i)
 	if @contact
 		erb :edit_contact
+	else
+		raise Sinatra::NotFound
+	end
+end
+
+put '/contacts/:id' do
+	@contact = @@rolodex.find(params [:id].to_i)
+	if @contact
+		@contact.first_name = params[:first_name]
+		@contact.last_name = params[:last_name]
+		@contact.email = params[:email]
+		@contact.note = params[:note]
+		redirect to("/contacts")
 	else
 		raise Sinatra::NotFound
 	end
